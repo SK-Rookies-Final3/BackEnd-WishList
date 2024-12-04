ab264549720f469867b23cbd9ad2bee71672536c
woo <clwkwhdk@gmail.com>
2024-12-04 10:01:32 +0900

    "fix: DB 미생성"

--- src/main/java/com/wishlist/db/ProductCode.java
+++ src/main/java/com/wishlist/db/ProductCode.java
@@ -1,9 +1,15 @@
 package com.wishlist.db;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductCode {
 
     @Id
--- src/main/java/com/wishlist/db/Wishlist.java
+++ src/main/java/com/wishlist/db/Wishlist.java
@@ -1,9 +1,17 @@
 package com.wishlist.db;
 
+
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wishlist {
 
     @Id
